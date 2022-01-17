1c2c8acf0606695e34426a825b09d37a8dd588bc
Faiyaz Rafeek <faiyazah_ct16014@stu.kln.ac.lk>
2022-01-17 17:52:45 +0530

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a0cc214e9693496ac2920eb908206ea3'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '876104a4-b7bb-480e-b473-131bc4a14e60'
+};
